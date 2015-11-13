14c370a805e21b4875cdc14c74f980a032ee6b69
Sean Craswell <scraswell@gmail.com>
2015-11-12 23:17:21 -0400

    Initial commit.

---
+++ Test.cs
@@ -0,0 +1,21 @@
+﻿//using NUnit.Framework;
+//using System;
+//
+//using Craswell.WebScraping.Banking;
+//
+//namespace Craswell.WebScraping.UnitTests
+//{
+//    [TestFixture()]
+//    public class Test
+//    {
+//        [Test()]
+//        public void TestCase()
+//        {
+//            using (TangerineClient tc = new TangerineClient())
+//            {
+//                var t = tc.GetAccounts();
+//            }
+//        }
+//    }
+//}
+//
